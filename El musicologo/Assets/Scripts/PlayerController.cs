1a893352db1abd02c1c6965276d333c386030ee4
SamCespedes <oscar.cespedes03@inacapmail.cl>
2022-12-19 00:43:01 -0300

    Animator funcionando
    Animator funcionando

--- El musicologo/Assets/Scripts/PlayerController.cs
+++ El musicologo/Assets/Scripts/PlayerController.cs
@@ -4,88 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalMove;
-    public float verticalMove;
-    
-    private Vector3 PlayerInput;
-   
-    public CharacterController Player;
-  
-    public float PlayerSpeed; 
-    public Camera mainCamera;
-    private Vector3 camForward;
-    private Vector3 camRight;
-    private Vector3 movePlayer;
-    public float gravity = 9.8f;
-    public float fallVelocity;
-    public float jumpForce;
+    private CharacterController characterController;
+    private Animator animator;
+    public new Transform camera;  
+    public float speed= 4;
+    public float gravity = -9.8f;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        Player = GetComponent<CharacterController>();
+        characterController = GetComponent <CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");
-        PlayerInput = new Vector3(horizontalMove, 0, verticalMove);
-        PlayerInput = Vector3.ClampMagnitude(PlayerInput, 1);
+        float hor = Input.GetAxis ("Horizontal");
+        float ver = Input.GetAxis ("Vertical");
+        Vector3 movement= Vector3.zero;
+        float movementSpeed = 0;
 
-        camDirection();
-
-        movePlayer= PlayerInput.x * camRight + PlayerInput.z * camForward;
-
-        movePlayer = movePlayer * PlayerSpeed;
-
-        Player.transform.LookAt(Player.transform.position + movePlayer);
-
-        SetGravity();
-
-        PlayerSkills();
-
-
-
-        Player.Move(movePlayer * Time.deltaTime);
-
-        SetGravity();
-    }
-
-    void camDirection()
-    {
-        camForward = mainCamera.transform.forward;
-        camRight = mainCamera.transform.right;
-
-        camForward.y= 0;
-        camRight.y = 0;
-
-        camForward = camForward.normalized;
-        camRight = camRight.normalized;
-    }
-
-    public void PlayerSkills()
-    {
-        if (Player.isGrounded && Input.GetButtonDown("Jump"))
+        if (hor != 0 || ver !=0)
         {
-            fallVelocity = jumpForce;
-            movePlayer.y = fallVelocity;
+            Vector3 forward = camera.forward;
+            forward.y= 0;
+            forward.Normalize();
+
+            Vector3 right = camera.right;
+            right.y = 0;
+            right.Normalize();
+
+            Vector3 direction= forward * ver + right * hor;
+            movementSpeed= Mathf.Clamp01(direction.magnitude);
+            direction.Normalize();
+            movement = direction * speed * movementSpeed * Time.deltaTime;
+
+            transform.rotation= Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.2f);
         }
-    }
-
-
-    void SetGravity()
-    {
-        if (Player.isGrounded)
-        {
-            fallVelocity = -gravity * Time.deltaTime;
-            movePlayer.y = fallVelocity;
-        }else
-        {
-            fallVelocity -= gravity * Time.deltaTime;
-            movePlayer.y = fallVelocity;
-
-        }
+       
+       movement.y += gravity * Time.deltaTime;
+       characterController.Move(movement);
+       animator.SetFloat("Speed", movementSpeed);
     }
 }
